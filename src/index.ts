a2e0ceb3fe6c07d664d539db4f132d9f8992126e
Cassiano Junior <cassianojuniorww@gmail.com>
2022-12-06 16:05:40 -0300

    adding cors and change port on app

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
+import cors from 'cors';
 import express, { Request, Response } from 'express';
 
 const app = express();
-
+app.use(cors());
 app.get('/', (req: Request, res: Response) => {
   const randomNumber = Math.floor(Math.random() * 10);
   res.status(200).json({ number: randomNumber });
 });
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(
+  app.listen(process.env.PORT || 3333, () => console.log('Server started'))
+);
